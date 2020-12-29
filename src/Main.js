2a8be3df0649fa01a49def96317a4afb5f3e1b85
GuentonBrooks <guenton.brooks@ibis.management.com>
2020-12-29 15:59:30 -0400

    Started work on main background

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,33 @@
+/*
+
+The Main.js Exports a React-Native Functional Component
+- Component is connected not to Redux
+
+- Main is the Main entry point of the visual part of this Application
+- All Screen Components will be loaded directly into this component
+
+---> TL;DR React Native Component for Application Logo <---
+
+*/
+
+// Import React Dependencies
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SkylineBG from './components/images/SkylineBG';
+import { ScaledSheet } from 'react-native-size-matters';
+
+// Import Components
 
 const Main = () => (
-  <View style={styles.container}>
-    <Text>MapSpeed!</Text>
-    <StatusBar style="auto" />
-  </View>
+    <SkylineBG /> 
+  // <View style={styles.container}>
+  //   <Text>MapSpeed!</Text>
+  //   <StatusBar style="auto" />
+  // </View>
 );
 
-const styles = StyleSheet.create({
+const styles = ScaledSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
---
+++ src/components/animations/FadeInView.js
@@ -0,0 +1,35 @@
+/*
+
+The FadeInView.js Exports a React-Native Functional Component
+- Component is connected not to Redux
+
+- FadeInView is an Animated.View that will fade-in it's children components
+- Animation props.duration can be passed in milliseconds, else defaults to 5000 (5seconds)
+
+---> TL;DR FadeIn Animation Container <---
+
+*/
+
+// Import React Dependencies
+import React, { useRef, useEffect } from 'react';
+import { Animated } from 'react-native';
+
+const FadeInView = (props) => {
+  // Init fade Opacity to 0;
+  const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
+
+  // Set Duration to props.duration or default to 5 seconds
+  const duration = props.duration ? props.duration : 5000;
+
+  // Set Anumation Timing Properties
+  const timing = { toValue: 1, duration, useNativeDriver: true };
+  
+  // Animation effect
+  useEffect(() => Animated.timing(fadeAnim, timing).start(), [fadeAnim]);
+
+  return (
+    <Animated.View style={{ ...props.style, opacity: fadeAnim }}>{props.children}</Animated.View>
+  );
+};
+
+export default FadeInView;
