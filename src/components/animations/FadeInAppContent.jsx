a1e6bc51b6658aa77551e9d583f5cc66a492f3f0
Guenton Brooks <Guenton@Gmail.com>
2022-12-20 16:02:53 -0400

    pre merge

--- src/components/animations/FadeInAppContent.jsx
+++ src/components/animations/FadeInAppContent.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { View } from 'react-native';
 
 import AppContent from '../containers/AppContent';
 import FadeInView from './FadeInView';
 
 const FadeInAppContent = ({ children }) => (
-  <AppContent>
+  <View style={{ flex: 1, zIndex: 6, elevation: 6 }}>
     <FadeInView style={{ flex: 1 }}>{children}</FadeInView>
-  </AppContent>
+  </View>
 );
 
 export default FadeInAppContent;
