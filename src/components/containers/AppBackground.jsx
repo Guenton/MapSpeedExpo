1f318961750b6afb6126542b908312a7127aebf7
Guenton Brooks <guenton.brooks@ibis-management.com>
2022-12-20 12:14:36 -0400

    fixed broken image typo

--- src/components/containers/AppBackground.jsx
+++ src/components/containers/AppBackground.jsx
@@ -3,8 +3,8 @@ import { ImageBackground, StatusBar } from 'react-native';
 import { useSelector } from 'react-redux';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-const skylineBG = require('../../assets/images/skylineBG.jpg');
-const hdBMW = require('../../assets/images/hdBMW.jpg');
+import skylineBG from '../../assets/images/SkylineBG.jpg';
+import hdBMW from '../../assets/images/hdBMW.jpg';
 
 const AppBackground = ({ children, skyline }) => {
   const primaryOpacity = useSelector((state) => state.color.primaryOpacity);
