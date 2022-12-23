397775b8e57fe37a53b89e5bce6b9ab3b876359a
Guenton Brooks <guenton.brooks@ibis-management.com>
2022-12-23 13:06:44 -0400

    fixed dotenv bug

--- src/firebase/app.js
+++ src/firebase/app.js
@@ -1,17 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
-
-console.log('api key is: ' + process.env.API_KEY);
+import Constants from 'expo-constants';
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: Constants.expoConfig.extra.API_KEY,
+  authDomain: Constants.expoConfig.extra.AUTH_DOMAIN,
+  databaseURL: Constants.expoConfig.extra.DATABASE_URL,
+  projectId: Constants.expoConfig.extra.PROJECT_ID,
+  storageBucket: Constants.expoConfig.extra.STORAGE_BUCKET,
+  messagingSenderId: Constants.expoConfig.extra.MESSAGING_SENDER_ID,
+  appId: Constants.expoConfig.extra.APP_ID,
+  measurementId: Constants.expoConfig.extra.MEASUREMENT_ID,
 };
 
 // Initialize Firebase App and Database
